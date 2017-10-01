ee9d8488a571ef7e32b6035b31fafa6259d6cc73
Chris Jarvis <jrvscm@gmail.com>
2017-09-30 22:58:40 -0600

    search function complete. pulling in brewery data

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
-
-function handleSubmit() {
-
-$('form').off().on('click', '.search-button', event => {
-	event.preventDefault();
-	
-	}	
+function watchSubmit() {
+	$('.form-container').off().on('click', '#search-button', event=> {
+		event.preventDefault();
+		$('.form-container').removeClass('center').css('height', '80%');
+		const query = $('#query').val();
+		getBreweryInfo(query);
+		$('#query').val('');
+	});
 }
+
+
+function getBreweryInfo(query) {
+	$.ajax({
+		method: "GET",
+		url: `https://cors-anywhere.herokuapp.com/http://api.brewerydb.com/v2/locations`,
+		dataType: 'json',
+		data: {
+			locality: `${query}`,
+			key: `130345cc1aee53c8eea5de3039902cc1`
+		},
+		success: function(data) {
+			console.log(data);
+		}
+	});	
+}
+
+
+function handleTour() {
+watchSubmit();
+}
+
+$(handleTour);
