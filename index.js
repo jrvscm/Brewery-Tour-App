4120b5a5c82eb9ee8e8d522b0cdca3c34cb31293
Chris Jarvis <jrvscm@gmail.com>
2017-10-01 15:28:32 -0600

    search function complete. pulls in data from apis

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 function watchSubmit() {
 	$('.form-container').off().on('click', '#search-button', event=> {
 		event.preventDefault();
-		$('.form-container').removeClass('center').css('height', '80%');
+		$('.form-container').removeClass('center').css('height', '100%').css('width', '250px').css('padding-top', '2%');
 		const query = $('#query').val();
-		getBreweryInfo(query);
+		//getBreweryInfo(query);
 		$('#query').val('');
 	});
 }
@@ -19,14 +19,65 @@ function getBreweryInfo(query) {
 			key: config.BREWERYDB_KEY
 		},
 		success: function(data) {
-			console.log(data);
-		}
-	});	
+				console.log(data);
+				const breweryInfo = data.data.map((data, index) => {
+				const breweryName = data.brewery.name;
+				const breweryLocation = data.latitude+data.longitude;
+			});
+	} 
+});
 }
+
+function getMapData () {
+
+$.ajax({
+		method: "GET",
+		url: `https://maps.googleapis.com/maps/api/js?`,
+		dataType: 'jsonp',
+		data: {
+			key: config.GOOGLEMAPS_KEY
+		},
+		success: function(data) {
+			console.log('success');
+			initMap();
+	}
+});
+
+}
+
+
+function initMap() {
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 14,
+          center: {lat:44.2911111,lng:-105.5016667}
+        });
+        var geocoder = new google.maps.Geocoder();
+        document.getElementById('search-button').addEventListener('click', function() {
+          geocodeAddress(geocoder, map);
+        });
+        console.log('initMap() ran')
+      }
+
+      function geocodeAddress(geocoder, resultsMap) {
+        var address = document.getElementById('query').value;
+        geocoder.geocode({'address': address}, function(results, status) {
+          if (status === 'OK') {
+            resultsMap.setCenter(results[0].geometry.location);
+            var marker = new google.maps.Marker({
+              map: resultsMap,
+              position: results[0].geometry.location
+            });
+          } else {
+            alert('Geocode was not successful for the following reason: ' + status);
+          }
+        });
+        console.log('geoCodeAddress ran')
+      }
 
 
 function handleTour() {
 watchSubmit();
+getMapData();
 }
 
 $(handleTour);
