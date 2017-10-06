f3a15fa867ca47470841e3a1470afb50d68276e1
Chris Jarvis <jrvscm@gmail.com>
2017-10-06 10:49:43 -0600

    search function, routing complete. Working on transportation modes

--- index.js
+++ index.js
@@ -110,22 +110,48 @@ function watchMarkerClick(marker, infowindow, resultsMap) {
 	marker.addListener('click', function() {
 		infowindow.open(resultsMap, marker);
 		let breweryTitle = `${marker.title}`;	
-		let tourLatLng = `${marker.position}`;	
-			if(tour.indexOf(breweryTitle) == -1) {
-				tour.push(breweryTitle);
-				tour.push(tourLatLng);
-				renderTourItem(breweryTitle);
-			}
-	});
-	getRoute();
+		let tourLatLng = `${marker.position}`;
+			tourLatLng = tourLatLng.split('').slice(0 +1, tourLatLng.length -1).join('');	
+			console.log(tourLatLng)
+			
+				if(tour.indexOf(breweryTitle) == -1) {
+					tour.push(breweryTitle);
+					routeArr.push(tourLatLng);
+					renderTourItem(breweryTitle);
+				}
+		});
+	getRoute(resultsMap);
 }
 
-function getRoute() {
+function getRoute(resultsMap) {
+	let directionsService = new google.maps.DirectionsService;
+	let directionsDisplay = new google.maps.DirectionsRenderer;
 	$('.form-container').on('click','.route', event => {
-		for(i=1; i<tour.length; i+=2) {
-			routeArr.push(tour[i]);
+			directionsDisplay.setMap(resultsMap);
+				calculateAndDisplayRoute(directionsService,directionsDisplay);
+	});
+}
+
+function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+	let waypts = [];
+	for(let i=0; i<routeArr.length; i++) {
+		waypts.push({
+			location: routeArr[i],
+		});
+	}
+
+	directionsService.route({
+		origin: routeArr[0],
+		destination: routeArr[routeArr.length -1],
+		waypoints: waypts,
+		optimizeWaypoints: true,
+		travelMode:'WALKING',
+	}, function(response, status) {
+		if(status ==='OK') {
+			directionsDisplay.setDirections(response);
+		} else {
+			window.alert('Directions request failed due to' + status);
 		}
-		console.log(routeArr)
 	});
 }
 
@@ -153,6 +179,9 @@ function watchSubmit() {
         $('.form-container').removeClass('center').css('height', '100%').css('width', '250px').css('padding-top', '2%');
         const query = $('#query').val();
         markers = [];
+        routeArr = [];
+        tour = [];
+        waypts = []
         getBreweryInfo(query);
         $('.tour').empty();
         $('#query').val('');
